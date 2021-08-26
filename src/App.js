d9c90caa45b443fa83ca293e32a21bbd5e6f89ad
Gilson Oliveira <gilsgil@gmail.com>
2021-08-26 20:41:07 -0300

    new page shop

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import "./App.css";
 import HomePage from "./pages/homepage/homepage";
 import { Route, Switch } from "react-router-dom";
-
-const HatsPage = () => <h1>HATS PAGE</h1>;
-
-const JacketsPage = () => <h1>JACKETS PAGE</h1>
+import ShopPage from "./pages/shop/shop";
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route exact path="/hats" component={HatsPage} />
-        <Route exact path="/jackets" component={JacketsPage} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
